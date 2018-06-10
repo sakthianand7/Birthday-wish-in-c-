ccb5095aa666e4dff33665d3440bdb21e0ddaa3f
P.SAKTHI ANAND <33320538+sakthianand7@users.noreply.github.com>
2018-06-10 15:23:27 +0530

    added dsa.cpp

---
+++ dsa.cpp
@@ -0,0 +1,55 @@
+#include<iostream.h>
+#include<conio.h>
+#include<dos.h>  
+
+int main()
+{
+
+ char h=3;
+ clrscr();
+ cout<<"\t\t\tFrom: "name"\n";
+ delay(2000);
+ cout<<"\t\t\tTo  : "name"\n\n";
+
+ delay(1000);
+ cout<<"\t\t\t\t* * * * *\n";
+
+ delay(2000);
+ cout<<"\t\t\t\t| | | | |\n";
+
+ delay(2000);
+ cout<<"\t\t\t      __|_|_|_|_|__\n";
+
+ delay(2000);
+ cout<<"\t\t\t     |"<<h<<"           "<<h<<"|\n";
+
+ delay(2000);
+ cout<<"\t\t\t   __| "<<h<<"  date  "<<h<<" |__\n";
+
+ delay(2000);
+ cout<<"\t\t\t  | "<<h<<"   "<<h<<"       "<<h<<"   "<<h<<" |\n";
+
+ delay(2000);
+ cout<<"\t\t\t__|"<<h<<"   HAPPY 18th"<<h<<"   "<<h<<"|__\n";
+
+ delay(2000);
+ cout<<"\t\t       | "<<h<<"   "<<h<<"     BIRTHDAY    "<<h<<" |\n";
+
+ delay(2000);
+ cout<<"\t\t       |"<<h<<"  "<<h<<"     "<<h<<"    "<<h<<"     "<<h<<"   "<<h<<"|\n";
+
+
+ delay(2000);
+ cout<<"\t\t       | "<<h<<" "<<h<<" "<<h<<" "<<h<<" Name"<<h<<" "<<h<<" "<<h<<" "<<h<<"  |\n";
+
+ delay(2000);
+ cout<<"\t\t       |_________________________|\n";
+
+ delay(2000);
+ cout<<"\t\tHOPE YOU WOULD HAVE A WONDERFUL BDAY WITH US\n"; 
+
+ cout<<"\n\n";
+ 
+ getch();
+ return 0;
+}
